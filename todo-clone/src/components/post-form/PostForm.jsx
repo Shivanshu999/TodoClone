6e8977655301c6e6647613de483e2c9946ce62fe
Shivanshu999 <shivanshuawasthi4@gmail.com>
2024-04-04 19:17:07 +0530

    todo-clone

---
+++ todo-clone/src/components/post-form/PostForm.jsx
@@ -0,0 +1,171 @@
+import React, { useCallback } from "react";
+import { useForm } from "react-hook-form";
+import { Button, Input, RTE, Select } from "..";
+import authService from "../../appwrite/config";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+export default function PostForm({ post }) {
+    const { register, handleSubmit, watch, setValue, control, getValues } = useForm({
+        defaultValues: {
+            title: post?.title || "",
+            slug: post?.$id || "",
+            content: post?.content || "",
+            status: post?.status || "active",
+        },
+    });
+
+    const navigate = useNavigate();
+    const userData = useSelector((state) => state.auth.userData);
+    const user = useSelector((state) => state.auth.status);
+    console.log(user);
+    console.log(userData)
+    // const submit = async (data) => {
+    //     console.log("hey");
+    //     console.log(data);
+    //     if (post) {
+    //         const file = data.image[0] ? await appwriteauthService.uploadFile(data.image[0]) : null;
+
+    //         if (file) {
+    //             appwriteauthService.deleteFile(post.featuredimage);
+    //         }
+
+    //         const dbPost = await appwriteauthService.updatePost(post.$id, {
+    //             ...data,
+    //             featuredimage: file ? file.$id : undefined,
+    //         });
+
+    //         if (dbPost) {
+    //             navigate(`/post/${dbPost.$id}`);
+    //         }
+    //     } else {
+    //         const file = await appwriteauthService.uploadFile(data.image[0]);
+
+    //         if (file) {
+    //             const fileId = file.$id;
+    //             data.featuredimage = fileId;
+    //             const dbPost = await appwriteauthService.createPost({ ...data, user: userData.$id });
+
+    //             if (dbPost) {
+    //                 navigate(`/post/${dbPost.$id}`);
+    //             }
+    //         }
+    //     }
+    // };
+    const editpost = async(data) => {
+
+        const file = data.image[0]
+          ? await authService.uploadFile(data.image[0])
+          : null;
+        if (file) {
+          authService.deleteFile(post.featuredimage);
+        }
+        const dbPost = await authService.updatePost(post.$id, {
+          ...data,
+          featuredimage: file ? file.$id : undefined,
+        });
+        console.log(dbPost);
+        if (dbPost) {
+          navigate(`/post/${dbPost.$id}`);
+        }
+    }
+  
+    const addPost = async (data) => {
+        console.log(data);
+      console.log("addPost")
+        const file = await authService.uploadFile(data.image[0]);
+        console.log(file);
+        if (file) {
+          const fileId = file.$id;
+          console.log(fileId, "hello");
+          data.featuredimage = fileId;
+          const dbPost = await authService.createPost({
+            ...data,
+            featuredimage: data.featuredimage,
+            user: userData.$id,
+          });
+          console.log({ ...data });
+          console.log(dbPost);
+          console.log(userData.$id);
+  
+          if (dbPost) {
+            navigate(`/post/${dbPost.$id}`);
+          }
+        }
+        
+      
+    }; 
+    const slugTransform = useCallback((value) => {
+        if (value && typeof value === "string")
+            return value
+                .trim()
+                .toLowerCase()
+                .replace(/[^a-zA-Z\d\s]+/g, "-")
+                .replace(/\s/g, "-");
+
+        return "";
+    }, []);
+
+    React.useEffect(() => {
+        const subscription = watch((value, { name }) => {
+            if (name === "title") {
+                setValue("slug", slugTransform(value.title), { shouldValidate: true });
+            }
+        });
+            
+        return () => subscription.unsubscribe();
+    }, [watch, slugTransform, setValue]);
+    
+    // const handleForm = ()=>{
+    //     console.log("clicked");
+    // } 
+    return (
+        <form onSubmit={handleSubmit(post?editpost:addPost)} className="flex flex-wrap">
+            <div className="w-2/3 px-2">
+                <Input
+                    label="Title :"
+                    placeholder="Title"
+                    className="mb-4"
+                    {...register("title", { required: true })}
+                />
+                <Input
+                    label="Slug :"
+                    placeholder="Slug"
+                    className="mb-4"
+                    {...register("slug", { required: true })}
+                    onInput={(e) => {
+                        setValue("slug", slugTransform(e.currentTarget.value), { shouldValidate: true });
+                    }}
+                />
+                {/* <RTE label="Content :" name="content" control={control} defaultValue={getValues("content")} /> */}
+            </div>
+            <div className="w-1/3 px-2">
+                <Input
+                    label="Featured Image :"
+                    type="file"
+                    className="mb-4"
+                    accept="image/png, image/jpg, image/jpeg, image/gif"
+                    {...register("image", { required: !post })}
+                />
+                {post && (
+                    <div className="w-full mb-4">
+                        <img
+                            src={authService.getFilePreview(post.featuredimage)}
+                            alt={post.title}
+                            className="rounded-lg"
+                        />
+                    </div>
+                )}
+                <Select
+                    options={["active", "inactive"]}
+                    label="Status"
+                    className="mb-4"
+                    {...register("status", { required: true })}
+                />
+                <Button type="submit" bgColor={post ? "bg-green-500" : undefined} className="w-full">
+                    {post ? "Update" : "Submit"}
+                </Button>
+            </div>
+        </form>
+    );
+}
---
+++ todo-clone/src/store/authSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    status: false,
+    userData: null
+}
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            console.log(action.payload.userData);
+            state.status = true;
+            state.userData = action.payload.userData;
+        },
+        logout: (state) => {
+            state.status = false;
+            state.userData = null;
+        } 
+
+    }
+})
+
+export const {login, logout} = authSlice.actions;
+console.log(login);
+
+export default authSlice.reducer
